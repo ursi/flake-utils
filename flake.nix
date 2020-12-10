03350f0f46682afa1f2efa59768730b621d97e74
Mason Mackaman <masondeanm@aol.com>
2020-12-10 15:54:39 -0500

    Add simple-js

--- flake.nix
+++ flake.nix
@@ -1,5 +1,34 @@
 {
-  outputs = _: {
+  outputs = { self, flake-utils }: {
+    builders.simple-js = {
+      name,
+      version,
+      nixpkgs,
+      path,
+      systems ? flake-utils.lib.defaultSystems,
+    }:
+      flake-utils.lib.eachSystem systems
+        (system:
+          with nixpkgs.legacyPackages.${system};
+
+          {
+            defaultPackage = stdenv.mkDerivation {
+                inherit path;
+                pname = name;
+                version = version;
+                buildInputs = [ nodejs ];
+                dontUnpack = true;
+
+                installPhase = ''
+                  mkdir -p $out/bin
+                  local ex=$out/bin/${name}
+                  cp $path $ex
+                  chmod +x $ex
+                '';
+              };
+          }
+        );
+
     makeFlakePackages = system: inputs: self: super:
       let
         fold = pkgs: name:
