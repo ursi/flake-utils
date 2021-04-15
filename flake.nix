f6047c2a39c87e0f27527f70e624f56bc71a4b31
Mason Mackaman <masondeanm@aol.com>
2021-04-14 20:58:10 -0400

    Formatting

--- flake.nix
+++ flake.nix
@@ -37,88 +37,88 @@
           write-js-script-bin = name: js: write-js-file { inherit name js; destination = "/bin/${name}"; };
         };
 
-    /* Make an outputs object out of a lambda of type `{ mkShell, pkgs, system } -> set`
+      /* Make an outputs object out of a lambda of type `{ mkShell, pkgs, system } -> set`
 
-       Example:
-         { outputs = { nixpkgs, utils, ... }:
-             utils.defaultSystems
-               ({ mkShell, pkgs, ... }:
-                  { devShell =
-                      mkShell
-                        { packages = with pkgs; [ a b c.d ];
-                          devshell.startup.main.text = ''echo "Hello, World!"'';
-                        };
-                  }
+         Example:
+           { outputs = { nixpkgs, utils, ... }:
+               utils.defaultSystems
+                 ({ mkShell, pkgs, ... }:
+                    { devShell =
+                        mkShell
+                          { packages = with pkgs; [ a b c.d ];
+                            devshell.startup.main.text = ''echo "Hello, World!"'';
+                          };
+                    }
+                 )
+                 nixpkgs
+           }
+      */
+      defaultSystems = mkOutputs: nixpkgs:
+        flake-utils.lib.eachDefaultSystem
+          (system:
+             mkOutputs
+               (let
+                  set =
+                    { pkgs = nixpkgs.legacyPackages.${system};
+                      inherit system;
+                    };
+                in
+                { inherit (import devshell set) mkShell; } // set
                )
-               nixpkgs
-         }
-    */
-    defaultSystems = mkOutputs: nixpkgs:
-      flake-utils.lib.eachDefaultSystem
-        (system:
-           mkOutputs
-             (let
-                set =
-                  { pkgs = nixpkgs.legacyPackages.${system};
-                    inherit system;
-                  };
-              in
-              { inherit (import devshell set) mkShell; } // set
-             )
-        );
+          );
 
-    /*  Returns an array of attributes based off path strings
+      /*  Returns an array of attributes based off path strings
 
-        Example:
-          attrsByPaths [ "a" "b" "c.d"] x
-          => [ x.a x.b x.c.d ]
-          attrsByPaths [ "a" "b" "c.d"] { a = 1; b = 2; }
-          => throws
-    */
-    lib.attrsByPaths = paths: set:
-      let
-        getPath = set: path:
-          with nixpkgs.lib;
-          attrByPath
-            (splitString "." path)
-            (throw "attribute does not exist: ${path}")
-            set;
-      in
-      map (getPath set) paths;
+          Example:
+            attrsByPaths [ "a" "b" "c.d"] x
+            => [ x.a x.b x.c.d ]
+            attrsByPaths [ "a" "b" "c.d"] { a = 1; b = 2; }
+            => throws
+      */
+      lib.attrsByPaths = paths: set:
+        let
+          getPath = set: path:
+            with nixpkgs.lib;
+            attrByPath
+              (splitString "." path)
+              (throw "attribute does not exist: ${path}")
+              set;
+        in
+        map (getPath set) paths;
 
-    defaultPackages = system: inputs:
-      map
-        (name: inputs.${name}.defaultPackage.${system})
-        (builtins.attrNames inputs);
+      defaultPackages = system: inputs:
+        map
+          (name: inputs.${name}.defaultPackage.${system})
+          (builtins.attrNames inputs);
 
-    /* Make a nix shell out of a lambda of type `{ pkgs, system } -> set`
+      /* Make a nix shell out of a lambda of type `{ pkgs, system } -> set`
 
-       Example:
-         {
-           outputs = { nixpkgs, utils, ... }:
-             utils.mkShell
-               ({ pkgs, ... }:
-                  with pkgs;
-                  { buildInputs = [ a b c.d ];
-                    shellHook = ''echo "Hello, World!"'';
-                  }
-               )
-               nixpkgs
+         Example:
+           {
+             outputs = { nixpkgs, utils, ... }:
+               utils.mkShell
+                 ({ pkgs, ... }:
+                    with pkgs;
+                    { buildInputs = [ a b c.d ];
+                      shellHook = ''echo "Hello, World!"'';
+                    }
+                 )
+                 nixpkgs
 
-         }
-    */
-    mkShell = mkShell':
-      defaultSystems
-        ({ pkgs, ... }@args: { devShell = pkgs.mkShell (mkShell' args); });
+           }
+      */
+      mkShell = mkShell':
+        defaultSystems
+          ({ pkgs, ... }@args: { devShell = pkgs.mkShell (mkShell' args); });
 
-    /* Make a nix shell with the package names in a list
+      /* Make a nix shell with the package names in a list
 
-       Example:
-         { outputs = { nixpkgs, utils, ... }:
-             utils.simpleShell [ "a" "b" "c.d"] nixpkgs;
-         }
-    */
-    simpleShell = buildInputs:
-      mkShell ({ pkgs, ... }: { buildInputs = lib.attrsByPaths buildInputs pkgs; });
-  };
+         Example:
+           { outputs = { nixpkgs, utils, ... }:
+               utils.simpleShell [ "a" "b" "c.d"] nixpkgs;
+           }
+      */
+      simpleShell = buildInputs:
+        mkShell ({ pkgs, ... }: { buildInputs = lib.attrsByPaths buildInputs pkgs; });
+    };
 }
