c61e746aa8ba2f72272e965bcde61d762b74b0b2
Mason Mackaman <masondeanm@aol.com>
2022-01-21 00:10:55 -0500

    Add default-systems attribute
    It is a list of the systems used in `for-default-systems`

--- flake.nix
+++ flake.nix
@@ -6,7 +6,9 @@
   outputs = { flake-utils, make-shell, ... }:
     with builtins;
     rec
-    { for-default-systems = for-systems flake-utils.lib.defaultSystems;
+    { default-systems = flake-utils.lib.defaultSystems;
+
+      for-default-systems = for-systems default-systems;
 
       for-systems = systems: make-outputs: inputs:
                                            # ^ using `self` (for `self.inputs`) causes an infinite recursion
